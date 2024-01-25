6e880c8832993b4e769779f44bcffa478c06ecc0
papaji002 <ppro00012@gmail.com>
2024-01-25 23:07:34 +0530

    first commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,29 +17,39 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
   const generateImage = async () => {
-    if(form.prompt)
-{
-  try{
-    setGeneratingImg(true);
-    const response = await fetch('http://localhost:8080/api/v1/dalle',{
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({prompt:form.prompt}),
-  })
-  const data = await response.json();
-  setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-} catch (err) {
-  alert(err);
-} finally {
-  setGeneratingImg(false);
-}
-} else {
-alert('Please provide proper prompt');
-}
-};
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -51,11 +61,11 @@ alert('Please provide proper prompt');
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(form ),
+          body: JSON.stringify({...form }),
         });
 
         await response.json();
-        // alert('Success');
+        alert('Success');
         navigate('/');
       } catch (err) {
         alert(err);
@@ -67,22 +77,7 @@ alert('Please provide proper prompt');
     }
   };
 
-
-    
-  const handleChange =  (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  }
-
-  
-  const handleSurpriseMe =  () => {
-    const randomPrompt = getRandomPrompt(form.prompt);
-    setForm({ ...form, prompt: randomPrompt });
-
-  }
-
-
   return (
-
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
@@ -100,7 +95,7 @@ alert('Please provide proper prompt');
             handleChange={handleChange}
           />
 
-<FormField
+          <FormField
             labelName="Prompt"
             type="text"
             name="prompt"
@@ -110,6 +105,7 @@ alert('Please provide proper prompt');
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             { form.photo ? (
               <img
@@ -124,21 +120,22 @@ alert('Please provide proper prompt');
                 className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
-              {generatingImg && (
+
+            {generatingImg && (
               <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
                 <Loader />
               </div>
             )}
-              </div>
           </div>
-          
+        </div>
+
         <div className="mt-5 flex gap-5">
           <button
             type="button"
             onClick={generateImage}
             className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
-              {generatingImg ? 'Generating...' : 'Generate'}
+            {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
 
@@ -151,13 +148,9 @@ alert('Please provide proper prompt');
             {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
         </div>
+      </form>
+    </section>
+  );
+};
 
-          </form>
-
-
-</section>
-      
-  )
-}
-
-export default CreatePost
+export default CreatePost;
